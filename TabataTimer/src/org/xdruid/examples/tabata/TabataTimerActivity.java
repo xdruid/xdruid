de9a60a6fc246691fa4f817fdd1a850e0df0b349
Pavle Jonoski <jonoski.pavle@gmail.com>
2012-04-22 00:13:05 +0200

    Added layout for the default screen. Added example project - TabataTimer

---
+++ TabataTimer/src/org/xdruid/examples/tabata/TabataTimerActivity.java
@@ -0,0 +1,13 @@
+package org.xdruid.examples.tabata;
+
+import org.xdruid.ui.ActivityDispatcher;
+
+import android.os.Bundle;
+
+public class TabataTimerActivity extends ActivityDispatcher {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.xdruid);
+    }
+}
